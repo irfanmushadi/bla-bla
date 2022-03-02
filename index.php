ae531d4c45c542859256eac6766ee2beb5973647
irfan <irfanmushadi492@gmail.com>
2022-03-02 21:04:15 +0700

    punya saya

--- index.php
+++ index.php
@@ -4,15 +4,98 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>belajar php</title>
 </head>
 <body>
     <h1>HALLO</h1>
+    
     <?php
-        $fano = " hallo nama saya irfan mushadi";
-        echo $fano;
+    // AND
+     $ir=10; $fan=20;
+     echo " y = $ir, x = $fan.";
+     echo "<br>";
+     if ($x=10 AND $y =20)
+     {
+      echo "Benar <br>";
+     }else{
+      echo "Salah";
+     }
+   
+        ?>
+            <?php
+        // OR
+        $ir = 10; $fan =20;
+        echo " y = $ir, x= $fan.<br>";
+        if($ir==10 OR $fan==20)
+        {
+        echo "Benar";
+        }else{
+        echo "Salah";
+        }
+            ?>
+    <br>
+          <?php
+          //||
+        $har = "mendung";
+        if($har !== "mendung"){
+        echo "kemarau";
+        }else{
+        echo "hujan";
+        }
+       
 
         ?>
+    <?php
+
+
+    $a = 6;
+    $b = 2;
+    
+    // menggunakan operator relasi lebih besar
+    $c = $a > $b;
+    echo "$a > $b: $c";
+    echo "<hr>";
+    
+    // menggunakan operator relasi lebih kecil
+    $c = $a < $b;
+    echo "$a < $b: $c";
+    echo "<hr>";
+    
+    // menggunakan operator relasi lebih sama dengan
+    $c = $a == $b;
+    echo "$a == $b: $c";
+    echo "<hr>";
+    
+    // menggunakan operator relasi lebih tidak sama dengan
+    $c = $a != $b;
+    echo "$a != $b: $c";
+    echo "<hr>";
+    
+    // menggunakan operator relasi lebih besar sama dengan
+    $c = $a >= $b;
+    echo "$a >= $b: $c";
+    echo "<hr>";
+    
+    // menggunakan operator relasi lebih kecil sama dengan
+    $c = $a <= $b;
+    echo "$a <= $b: $c";
+    echo "<hr>";
+    ?>
+    <?php
+    $a = 25;
+    $b = 6;
+    $c = $a + $b;
+    echo "$a ditambah $b hasilnya $c<br />";
+    $c = $a - $b;
+    echo "$a dikurang $b hasilnya $c<br />";
+    $c = $a * $b;
+    echo "$a dikali $b hasilnya $c<br />";
+    $c = $a / $b;
+    echo "$a dibagi $b hasilnya $c<br />";
+    $c = $a % $b;
+    echo "$a dibagi $b sisanya $c<br />";
+    echo "Di akhir program, a=$a, b=$b, c=$c";
+    ?>
 
 </body>
 </html>
