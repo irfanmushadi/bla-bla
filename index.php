1a22e1a23487e31e29414776b97332021910be86
irfan <irfanmushadi492@gmail.com>
2022-03-02 21:48:18 +0700

    switch

--- index.php
+++ index.php
@@ -10,92 +10,22 @@
     <h1>HALLO</h1>
     
     <?php
-    // AND
-     $ir=10; $fan=20;
-     echo " y = $ir, x = $fan.";
-     echo "<br>";
-     if ($x=10 AND $y =20)
-     {
-      echo "Benar <br>";
-     }else{
-      echo "Salah";
-     }
-   
-        ?>
-            <?php
-        // OR
-        $ir = 10; $fan =20;
-        echo " y = $ir, x= $fan.<br>";
-        if($ir==10 OR $fan==20)
-        {
-        echo "Benar";
-        }else{
-        echo "Salah";
-        }
-            ?>
-    <br>
-          <?php
-          //||
-        $har = "mendung";
-        if($har !== "mendung"){
-        echo "kemarau";
-        }else{
-        echo "hujan";
-        }
-       
-
-        ?>
-    <?php
-
-
-    $a = 6;
-    $b = 2;
-    
-    // menggunakan operator relasi lebih besar
-    $c = $a > $b;
-    echo "$a > $b: $c";
-    echo "<hr>";
-    
-    // menggunakan operator relasi lebih kecil
-    $c = $a < $b;
-    echo "$a < $b: $c";
-    echo "<hr>";
-    
-    // menggunakan operator relasi lebih sama dengan
-    $c = $a == $b;
-    echo "$a == $b: $c";
-    echo "<hr>";
-    
-    // menggunakan operator relasi lebih tidak sama dengan
-    $c = $a != $b;
-    echo "$a != $b: $c";
-    echo "<hr>";
-    
-    // menggunakan operator relasi lebih besar sama dengan
-    $c = $a >= $b;
-    echo "$a >= $b: $c";
-    echo "<hr>";
-    
-    // menggunakan operator relasi lebih kecil sama dengan
-    $c = $a <= $b;
-    echo "$a <= $b: $c";
-    echo "<hr>";
-    ?>
-    <?php
-    $a = 25;
-    $b = 6;
-    $c = $a + $b;
-    echo "$a ditambah $b hasilnya $c<br />";
-    $c = $a - $b;
-    echo "$a dikurang $b hasilnya $c<br />";
-    $c = $a * $b;
-    echo "$a dikali $b hasilnya $c<br />";
-    $c = $a / $b;
-    echo "$a dibagi $b hasilnya $c<br />";
-    $c = $a % $b;
-    echo "$a dibagi $b sisanya $c<br />";
-    echo "Di akhir program, a=$a, b=$b, c=$c";
-    ?>
+$nilai = 90;
+switch ($nilai) {
+ case 100:
+  echo "Nilai A";
+  break;
+ case 90:
+  echo "Nilai B";
+  break;
+ case 80:
+  echo "Nilai C";
+  break;
+ default:
+  echo "Nilai Tidak ditemukan";
+  break;
+}
+?>
 
 </body>
 </html>
