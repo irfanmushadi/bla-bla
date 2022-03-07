d0e44082953f5bbb2b7a0e6a7c21ce3fd5987802
irfan <irfanmushadi492@gmail.com>
2022-03-07 10:00:11 +0700

    tugas tanggal 7

---
+++ tugas array.php
@@ -0,0 +1,9 @@
+<?php 
+
+$veh = array('motor','mobil','kereta','kapal','pesawat');
+
+for($x=0;$x<count($veh);$x++){
+	echo $veh[$x]."<br/>";
+}
+ 
+?>
---
+++ tugas perulangan.php
@@ -0,0 +1,5 @@
+<?php 
+for($say=1;$say<=15;$say++){	
+	echo "Belajar coding lur !"."<br/>";
+}
+?>
