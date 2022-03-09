ecd1987dff8436cf6cefa977aae6aa21f7b89e0b
irfan <irfanmushadi492@gmail.com>
2022-03-09 21:32:35 +0700

    tugass oop

--- latihan.php
+++ latihan.php
@@ -1,21 +1,43 @@
-<?php 
-
-$mobil = 'Bagus'; 
-switch ($mobil)  { 
-    case 'Bagus': 
-        echo 'jika mesin dan body bagus'; 
-        break; 
-    case 'menengah': 
-        echo 'jika mesin bagus body lecet dikit '; 
-        break; 
-    case 'jelek': 
-         echo 'jika mesin jelek dan body jelek'; 
-        break; 
-    default: 
-        echo 'ini mobil ku'; 
-        break; 
-} 
-?>
-
 <?php
+class User
+{
+    public $name;
+    public $username;
+    protected $birthdate = '10 Juni 1999';
+    public $address = 'Bandung';
+    public $gender = 'Male';
+
+    public function showSalam(){
+        echo "Halo, nama saya $this->name <br/>";
+    }
+}
+
+class Bio extends User
+{
+    public $birthdate = ' 10 Januari 2010 ';
+
+    public function showBio(){
+        echo "Username : $this->username <br/>";
+        echo "Birthdate : $this->birthdate <br/>";
+        echo "Gender : $this->gender <br/>";
+    }
+
+    public function showAddress(){
+        echo "Address : $this->address";
+    }
+}
+
+$myBio = new Bio();
+
+$myBio->name = 'Anne';
+$myBio->username = 'anne';
+$myBio->gender = 'Female';
+
+echo $myBio->username . '<br/>';
+echo $myBio->gender;
+echo $myBio->birthdate . '<br/>';
+
+$myBio->showSalam();
+$myBio->showBio();
+$myBio->showAddress();
 
